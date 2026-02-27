20eb10eef2ad3022b8dd08aa1ea307675ae3c59d
rileysargent <rileysargentagain@gmail.com>
2026-02-28 03:08:27 +1100

    Update js/auth.js with Firebase authentication code

--- js/auth.js
+++ js/auth.js
@@ -1,16 +1,20 @@
-// Authentication logic
+// Firebase authentication code
 
-const authenticateUser = (username, password) => {
-    // Logic to authenticate user
-    if (username === 'admin' && password === 'password') {
-        return true;
-    }
-    return false;
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
-const logoutUser = () => {
-    // Logic to log out user
-    console.log('User logged out.');
-};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-module.exports = { authenticateUser, logoutUser };
+export const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+};
