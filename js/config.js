1c8f45f0b2f64f3397f885dda6da2d0a921d2c04
rileysargent <rileysargentagain@gmail.com>
2026-02-28 03:11:08 +1100

    Create js/config.js file with Firebase configuration

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AImnFlNS8LGyK0tza51jQ81V6ldWVv5riruKXsb",
+    authDomain: "hyperai-r.firebaseapp.com",
+    projectId: "hyperai-r",
+    storageBucket: "hyperai-r.appspot.com",
+    messagingSenderId: "129777724896",
+    appId: "1:129777724896:web:abc911893q056n59r78s85"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
